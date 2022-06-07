9327df01a406e483eb39cec436a2499d1eb25f38
Sweetel <Pablo@DESKTOP-JU3BHCH>
2022-06-08 00:32:07 +0200

    segundaprueba23

--- pruebagit/src/pruebagit/Pruebagit.java
+++ pruebagit/src/pruebagit/Pruebagit.java
@@ -14,7 +14,7 @@ public class Pruebagit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("hola masdsfundo");
     }
     
 }
