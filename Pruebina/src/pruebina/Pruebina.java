409e8c8f2d84f0e3458acce73d8b6ccc899aa078
Sweetel <Pablo@DESKTOP-JU3BHCH>
2022-06-08 00:10:24 +0200

    segundaprueba23

--- Pruebina/src/pruebina/Pruebina.java
+++ Pruebina/src/pruebina/Pruebina.java
@@ -15,7 +15,7 @@ public class Pruebina {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hello world");
+        System.out.println("hello sdasdadadasdworld");
     }
     
 }
