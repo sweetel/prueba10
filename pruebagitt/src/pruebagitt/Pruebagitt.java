3262aab7267dc900e4cd4394ce9ae065826cbd7a
Sweetel <Pablo@DESKTOP-JU3BHCH>
2022-06-15 21:06:32 +0200

    primera v ersion

---
+++ pruebagitt/src/pruebagitt/Pruebagitt.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebagitt;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Pruebagitt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+
+    
+        System.out.println("hola mundo"); 
+        System.out.println("punto dos");
+             
+    }
+    
+}
+    
+    
+
